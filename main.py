9abd8484d393658c18717da117da5b6f63837982
tmchek <pinanahima@gmail.com>
2025-01-23 23:29:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,15 @@ from func.SendMsg import (
                             SendMsg,
                             scrapp_ccs,
                             chk_bin
-                            
                              )
 
-from func.dates import ( API_HASD ,
+from func.dates import ( 
+                         API_HASD,
                          API_ID,
                          NUMERO,
                          APROVED,
                          DEAD,
                          TEXT,
-                         
                          )
 
 
@@ -31,7 +30,7 @@ async def handler(event):
         def __init__(self):
             self.getMsg = event.message.message.upper()  
             self.RexGex = r'\d{14,16}\|\d{1,2}\|\d{2,4}\|\d{3,4}'
-            self.ScrCss = re.sub(self.RexGex,r'<code>\g<0></code>',self.getMsg)
+            self.ScrCss = re.sub(self.RexGex, r'<code>\g<0></code>', self.getMsg)
 
         def Run(self):
             
@@ -48,21 +47,22 @@ async def handler(event):
                         
                         fin = time.perf_counter() - ini
 
-                        self.text =  TEXT.format(   bins[0], bins[1],bins[2],
-                                                    ccs[0], 
-                                                    ccs[1], 
-                                                    ccs[2], 
-                                                    ccs[3],
-                                                    ccs[0][0:6],
-                                                    bins[3],bins[4],bins[5],bins[6],
-                                                    
-                                                    f'{fin:0.4f}'
-                                                    )
+                        self.text = TEXT.format(
+                            bins[0], bins[1], bins[2],
+                            ccs[0], 
+                            ccs[1], 
+                            ccs[2], 
+                            ccs[3],
+                            ccs[0][0:6],
+                            bins[3], bins[4], bins[5], bins[6],
+                            f'{fin:0.4f}'
+                        )
                         
                         SendMsg().enviar(self.text)
                         return '[⌁] - Aprovada ✅'
                     
-                except: ...
+                except Exception as e:
+                    print(f"Error: {e}")
                     
 
     ResP = EventMsg().Run()
