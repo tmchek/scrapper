ef15f62762b6e9f7dc3f6bf9d6d979f356c5acd8
tmchek <pinanahima@gmail.com>
2025-01-24 00:59:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,56 +1,52 @@
 import re, os, time
 from telethon.sync import TelegramClient, events
-from dotenv import load_dotenv
+from dotenv import load_dotenv  # Importa la librería dotenv para leer el archivo .env
 
-from func.SendMsg import SendMsg, scrapp_ccs, chk_bin
-from func.dates import APROVED, DEAD, TEXT
-
-# Cargar variables del archivo .env
+# Cargar las variables del archivo .env
 load_dotenv()
 
-API_HASD = os.getenv("API_HASH")
-API_ID = int(os.getenv("API_ID"))
-NUMERO = os.getenv("PHONE_NUMBER")
+# Obtener las variables del entorno
+API_ID = os.getenv('API_ID')
+API_HASH = os.getenv('API_HASH')
+PHONE_NUMBER = os.getenv('PHONE_NUMBER')
+VERIFICATION_CODE = os.getenv('VERIFICATION_CODE')
 
-# Configurar el cliente de Telethon
-client = TelegramClient(f'ScrapAproved', API_ID, API_HASD)
-client.start(NUMERO)
+client = TelegramClient('new_session', API_ID, API_HASH)
+
+# Iniciar sesión automáticamente con el código de verificación
+client.start(phone=PHONE_NUMBER, code_callback=lambda: VERIFICATION_CODE)
 
 @client.on(events.MessageEdited())
 async def handler(event):
     ini = time.perf_counter()
-
     class EventMsg:
         def __init__(self):
-            self.getMsg = event.message.message.upper()
+            self.getMsg = event.message.message.upper()  
             self.RexGex = r'\d{14,16}\|\d{1,2}\|\d{2,4}\|\d{3,4}'
             self.ScrCss = re.sub(self.RexGex, r'<code>\g<0></code>', self.getMsg)
 
         def Run(self):
-            if re.search(self.RexGex, self.getMsg) and any(live in self.getMsg for live in DEAD):
-                scrapp_ccs(self.ScrCss)
+            if (re.search(self.RexGex, self.getMsg) and any(live in self.getMsg for live in DEAD) == True): 
+                ccs = scrapp_ccs(self.ScrCss)
                 return '[⌁] - Dead ❌'
-            else:
+            else:  
                 try:
-                    if re.search(self.RexGex, self.getMsg) and any(live in self.getMsg for live in APROVED):
+                    if (re.search(self.RexGex, self.getMsg) and any(live in self.getMsg for live in APROVED) == True): 
                         ccs = scrapp_ccs(self.ScrCss)
                         bins = chk_bin(ccs[0][0:6])
                         fin = time.perf_counter() - ini
-
                         self.text = TEXT.format(
                             bins[0], bins[1], bins[2],
-                            ccs[0],
-                            ccs[1],
-                            ccs[2],
+                            ccs[0], 
+                            ccs[1], 
+                            ccs[2], 
                             ccs[3],
                             ccs[0][0:6],
                             bins[3], bins[4], bins[5], bins[6],
                             f'{fin:0.4f}'
                         )
-
                         SendMsg().enviar(self.text)
                         return '[⌁] - Aprovada ✅'
-
                 except Exception as e:
                     print(f"Error: {e}")
 
@@ -59,7 +55,6 @@ async def handler(event):
 
 os.system("clear")
 banner = """   
-
  _   _           _              _ _         _____                    
 | | | |         | |            | | |       |_   _|                   
 | | | |_ __ ___ | |__  _ __ ___| | | __ _    | | ___  __ _ _ __ ___  
@@ -68,7 +63,6 @@ banner = """
  \___/|_| |_| |_|_.__/|_|  \___|_|_|\__,_|   \_/\___|\__,_|_| |_| |_|
                                                                      
                                                                      
-
  User:  
  Code by: 
 """
